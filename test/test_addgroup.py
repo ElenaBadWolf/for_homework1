583b51d8f70255097b0a17a3eb84cc5af6cb6a7e
ElenaBadWolf <plastmassovaya@gmail.com>
2016-03-19 18:25:22 +0300

    выделен класс для управления сессиями

--- test/test_addgroup.py
+++ test/test_addgroup.py
@@ -10,25 +10,23 @@ def app(request):
     request.addfinalizer(fixture.destroy)
     return fixture
 
-
 def test_test_addgroup(app):
         app.open_home_page()
-        app.login(username = "admin",password= "secret")
+        app.session.login(username = "admin",password= "secret")
         app.init_group_creation()
         app.fill_groups_form(Group(name="test", header="test",footer= "test"))
         app.submit_group_creation()
         app.returt_to_groups_page()
-        app.logout()
-
+        app.session.logout()
 
 def test_add_empty_group(app):
         app.open_home_page()
-        app.login(username = "admin",password= "secret")
+        app.session.login(username = "admin",password= "secret")
         app.init_group_creation()
         app.fill_groups_form(Group(name="", header="",footer= ""))
         app.submit_group_creation()
         app.returt_to_groups_page()
-        app.logout()
+        app.session.logout()
 
 
 
