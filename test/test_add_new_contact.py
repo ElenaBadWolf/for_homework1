fc4a6dca2e7903ba0fd8c7f13ec95dac29b3bca3
ElenaBadWolf <plastmassovaya@gmail.com>
2016-03-19 19:06:58 +0300

    выделен класс для работы с контактами

--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -12,18 +12,18 @@ def app(request):
 
 def test_add_new_contact(app):
     app.sessioncontact.login(username="admin",password="secret")
-    app.create_new_contact(Contact(firstname="fghjfgfgfg", middlename="gfgf", lastname="gfgf", nickname="gfg",
-                                            title="gfg", company="gfg", address="gfgf", home="gf", mobile="gfg",
-                                            work="gdfgfd",
-                                            fax="yuy", email2="kjhkhj", email3="kjhkjh", homepage="khjkjh",
-                                            byear="2005", ayear="2001", address2="gfdgfdg", phone2="jhjj",
-                                            notes="gfdgdf",))
+    app.contact.create(Contact(firstname="fghjfgfgfg", middlename="gfgf", lastname="gfgf", nickname="gfg",
+                               title="gfg", company="gfg", address="gfgf", home="gf", mobile="gfg",
+                               work="gdfgfd",
+                               fax="yuy", email2="kjhkhj", email3="kjhkjh", homepage="khjkjh",
+                               byear="2005", ayear="2001", address2="gfdgfdg", phone2="jhjj",
+                               notes="gfdgdf", ))
     app.return_to_home_page()
 
 def test_add_new_empty_contact(app):
     app.sessioncontact.login(username="admin",password="secret")
-    app.create_new_contact(Contact(firstname="", middlename="", lastname="", nickname="", title="", company="",
-                                            address="", home="", mobile="", work="gdfgfd",
-                                            fax="", email2="", email3="kjhkjh", homepage="khjkjh", byear="2005",
-                                            ayear="2001", address2="gfdgfdg", phone2="jhjj", notes="gfdgdf",))
+    app.contact.create(Contact(firstname="", middlename="", lastname="", nickname="", title="", company="",
+                               address="", home="", mobile="", work="gdfgfd",
+                               fax="", email2="", email3="kjhkjh", homepage="khjkjh", byear="2005",
+                               ayear="2001", address2="gfdgfdg", phone2="jhjj", notes="gfdgdf", ))
     app.return_to_home_page()
