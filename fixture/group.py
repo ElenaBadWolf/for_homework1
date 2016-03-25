8c351e9bb7e9ed8e380409a1f934ab5429140fc3
ElenaBadWolf <plastmassovaya@gmail.com>
2016-03-25 22:15:48 +0300

    условие if для модификации групп

--- fixture/group.py
+++ fixture/group.py
@@ -14,23 +14,26 @@ class GroupHelper:
     def fill_form( self, group ):
         # fill group form
         wd = self.app.wd
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group.footer", group.footer)
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
 
     def delete_first_group(self):
         wd = self.app.wd
-        # select first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group(wd)
         # submit deletion
         wd.find_element_by_name("delete").click()
 
+    def select_first_group(self, wd):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
 
     def init_creation(self):
         # init group creation
@@ -44,4 +47,14 @@ class GroupHelper:
         wd.find_element_by_name("group_name").send_keys("changes")
         wd.find_element_by_name("update").click()
 
+
+    def modify_first_group(self, new_group_data):
+        wd = self.app.wd
+        self.select_first_group(wd)
+        # open modification form
+        wd.find_element_by_name("edit").click()
+        # fill group form
+        self.fill_form(new_group_data)
+        # submit modification
+        wd.find_element_by_name("update").click()
 
