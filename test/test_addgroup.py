501f2d7fca6a0018c509fb43425f3eef733f2546
ElenaBadWolf <plastmassovaya@gmail.com>
2016-03-25 23:13:00 +0300

    повысили интеллектуальность фикстуры (группы)

--- test/test_addgroup.py
+++ test/test_addgroup.py
@@ -5,19 +5,15 @@ from model.group import Group
 
 def test_test_addgroup(app):
         app.open_home_page_group()
-        app.session.login(username = "admin",password= "secret")
         app.group.init_creation()
         app.group.fill_form(Group(name="test", header="test", footer="test"))
         app.group.submit_creation()
         app.group.returt_to_groups_page()
-        app.session.logout()
 
 def test_add_empty_group(app):
         app.open_home_page_group()
-        app.session.login(username = "admin",password= "secret")
         app.group.init_creation()
         app.group.fill_form(Group(name="", header="", footer=""))
         app.group.submit_creation()
         app.group.returt_to_groups_page()
-        app.session.logout()
 
