b88ba235d0cf5f7873bd72eb138a9d24c8565632
ElenaBadWolf <plastmassovaya@gmail.com>
2016-03-26 22:39:41 +0300

    предусловие для модификации групп

--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -1,4 +1,12 @@
-def test_delete_first_group(app):
+from model.group import Group
+
+def test_mod_group(app):
         app.open_home_page_group()
         app.session.login(username = "admin",password= "secret")
+        if app.group.count() == 0:
+           app.open_home_page_group()
+           app.group.init_creation()
+           app.group.fill_form(Group(name="test"))
+           app.group.submit_creation()
+           app.group.returt_to_groups_page()
         app.group.mod_group()
