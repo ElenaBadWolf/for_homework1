e39a36f701464e6a3d4fb41bbfd0d8f89f1bed9e
ElenaBadWolf <plastmassovaya@gmail.com>
2016-04-16 21:18:40 +0300

    загрузка информации из конфигурационного файла

--- conftest.py
+++ conftest.py
@@ -1,14 +1,19 @@
 import pytest
 from fixture.application import Application
+import json
+target = None
 
 @pytest.fixture
 def app(request):
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    fixture=Application(browser= browser, base_url = base_url)
+    if target is None:
+        with open(request.config.getoption("--target")) as config_file:
+            target = json.load(config_file)
+    fixture=Application(browser= browser, base_url = target['baseUrl'])
     request.addfinalizer(fixture.destroy)
     return fixture
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default = "firefox")
-    parser.addoption("--baseUrl", action="store", default = "http://localhost/addressbook/addressbook/")
+    parser.addoption("--target", action="store", default = "target.json")
