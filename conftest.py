78b21e36c77c44c9a33f5bfe945bc7a5d0c99ac4
ElenaBadWolf <plastmassovaya@gmail.com>
2016-04-16 23:56:30 +0400

    информация из конф.файла

--- conftest.py
+++ conftest.py
@@ -2,14 +2,16 @@ import pytest
 from fixture.application import Application
 import json
 target = None
+import os.path
 
 @pytest.fixture
 def app(request):
     global target
     browser = request.config.getoption("--browser")
     if target is None:
-        with open(request.config.getoption("--target")) as config_file:
-            target = json.load(config_file)
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
     fixture=Application(browser= browser, base_url = target['baseUrl'])
     request.addfinalizer(fixture.destroy)
     return fixture
