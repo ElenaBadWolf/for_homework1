bf10e943da237489d3afb0b43c0d4647faf27718
ElenaBadWolf <plastmassovaya@gmail.com>
2016-04-21 00:10:51 +0400

    ошибка в тесте

---
+++ test_addgroup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from model.group import Group
+
+
+
+def test_test_addgroup(app, json_groups):
+        group = json_groups
+        app.session.login(username="admin",password="secret")
+        app.open_home_page_group()
+        old_groups = app.group.get_group_list()
+        app.group.init_creation()
+        app.group.fill_form(group)
+        app.group.submit_creation()
+        app.group.return_to_groups_page()
+        assert len(old_groups) + 1 == app.group.count()
+        new_groups = app.group.get_group_list()
+        old_groups.append(group)
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key =Group.id_or_max)
+
+#def test_add_empty_group(app):
+        #app.session.login(username="admin",password="secret")
+        #app.open_home_page_group()
+        #old_groups = app.group.get_group_list()
+        #group = Group(name="", header="", footer="")
+        #app.group.init_creation()
+        #app.group.fill_form(group)
+        #app.group.submit_creation()
+        #app.group.returt_to_groups_page()
+        #new_groups = app.group.get_group_list()
+        #assert len(old_groups) + 1 == len(new_groups)
+        #old_groups.append(group)
+        #assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key =Group.id_or_max)
+
